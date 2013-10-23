6e8d8c4d7c411c4969c6f517dbda92cc660ddf3c
amutake <amutake.s@gmail.com>
2013-10-23 18:32:16 +0900

    Add remote-process example

---
+++ haskell/cloud-haskell/junk/remote-process.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE TemplateHaskell, ScopedTypeVariables #-}
+
+-- exec:
+-- % runghc remote-process.hs slave localhost 12000 & \
+--   runghc remote-process.hs slave localhost 12001 & \
+--   runghc remote-process.hs slave localhost 12002 & \
+--   runghc remote-process.hs master localhost 12003
+
+import Import
+
+import Control.Concurrent (threadDelay)
+import Control.Monad
+import System.Environment
+
+echo :: Process ()
+echo = do
+  (str :: String, pid) <- expect
+  say $ "got: " ++ str
+  send pid str
+  terminate
+
+remotable ['echo] -- 必要
+
+master :: Backend -> [NodeId] -> Process ()
+master backend slaves = do
+  say $ "slaves: " ++ show slaves
+  pids <- forM slaves $ \node -> spawn node $(mkStaticClosure 'echo)
+  say $ "pids: " ++ show pids
+  self <- getSelfPid
+  forM_ pids $ \pid -> send pid ("hello", self)
+  recv (length pids)
+  terminateAllSlaves backend
+  where
+    recv 0 = say "all received"
+    recv n = do
+      str :: String <- expect
+      say $ "recv: " ++ str
+      recv (n - 1)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let rtable = __remoteTable initRemoteTable -- 必要
+  case args of
+    ["master", host, port] -> do
+      liftIO $ threadDelay 100000
+      backend <- initializeBackend host port rtable
+      startMaster backend (master backend)
+    ["slave", host, port] -> do
+      backend <- initializeBackend host port rtable
+      startSlave backend
