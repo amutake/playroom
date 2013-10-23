f04c77fa1bd2993df9d34ce00f7345d0fc33ba11
amutake <amutake.s@gmail.com>
2013-10-23 13:09:22 +0900

    Modify Actor

--- haskell/actor/Actor.hs
+++ haskell/actor/Actor.hs
@@ -1,51 +1,85 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, RankNTypes #-}
 
 import Control.Concurrent
 import Control.Applicative
 import Control.Monad.IO.Class
 import Control.Monad.State
+import Control.Monad.Reader
 
-data Actor = Actor
-  { threadId :: ThreadId
-  , mailBox :: MVar [Message]
-  , actorName :: Name
+data Actor r = Actor
+  { mailBox :: MailBox r
   }
 
-newtype ActorWorld a = ActorWorld
-  { unWorld :: StateT Name IO a
+newtype ActorWorld r a = ActorWorld
+  { unWorld :: ReaderT (MailBox r) IO a
   } deriving
   ( Functor
   , Applicative
   , Monad
   , MonadIO
+  , MonadReader (MailBox r)
   )
 
-newtype Message = Message Name
+type ActorId a = ThreadId
 
-newtype Name = Name Int
+type Behavior a b = a -> ActorWorld b ()
 
-instance Show Name where
-  show (Name n) = "name" ++ show n
+type MailBox a = MVar [a]
 
-create :: ActorWorld () -> ActorWorld Actor
-create world = do
-  tid <- liftIO $ forkIO $ unWorld world
-  mvar <- liftIO newEmptyMVar
-  name <- getNewName
-  return (Actor tid mvar name)
+execWorld :: MailBox r -> ActorWorld r a -> IO a
+execWorld mbox world =
+  runReaderT (unWorld world) mbox
+
+emptyMailBox :: IO (MailBox a)
+emptyMailBox = newEmptyMVar
+
+create :: Behavior a r -> a -> IO (Actor r)
+create bdef a = do
+  mbox <- emptyMailBox
+  forkIO $ execWorld mbox $ bdef a
+  return $ Actor mbox
+
+send :: Actor a -> a -> forall b. ActorWorld b ()
+send actor msg = do
+  mbox <- ask
+  liftIO $ modifyMVar_ mbox $ \msgs ->
+    return $ msg : msgs
+
+bindMessage :: (r -> ActorWorld r ()) -> ActorWorld r ()
+bindMessage f = do
+  mbox <- ask
+  msgs <- liftIO $ takeMVar mbox
+  case msgs of
+    [] -> error "bind error: empty"
+    (msg : msgs) -> f msg
+
+getSelf :: ActorWorld r (Actor r)
+getSelf = do
+  mbox <- ask
+  return $ Actor mbox
+
+factorial :: Behavior () (Int, Actor Int)
+factorial () = bindMessage $ \(val, cust) ->
+  if (val == 0)
+    then send cust 1
+    else do
+      cont <- liftIO $ create factorialCont (val, cust)
+      self <- getSelf
+      send self (val - 1, cont)
+      factorial ()
+
+factorialCont :: Behavior (Int, Actor Int) Int
+factorialCont (val, cust) =
+  bindMessage $ \arg -> send cust (val * arg)
+
+main :: IO ()
+main = do
+  fact <- create factorial ()
+  e <- create end ()
+  create (go fact e) ()
+  return ()
  where
-  getNewName = do
-    Name n <- get
-    put $ Name $ n + 1
-    return (Name n)
-
-become :: ActorWorld a
-become = id
-
-send :: Name -> Message -> ActorWorld ()
-send name msg = do
-
--- receive :: ActorWorld Message
--- receive = do
---   actor <- ask
---   takeMVar $ mailBox actor
+  go fact e () = do
+    send fact (3, e)
+  end () = bindMessage $ \result -> do
+    liftIO $ print result
