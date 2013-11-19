7d9310d977fcc980d6d3131a7243b2f3fb94471a
amutake <amutake.s@gmail.com>
2013-11-19 17:08:16 +0900

    Add nsend-remote example

---
+++ haskell/cloud-haskell/small-examples/nsend-remote.hs
@@ -0,0 +1,43 @@
+import Import
+import Control.Distributed.Process.Internal.Types
+import Data.ByteString.Char8 (pack)
+import Network.Transport hiding (send)
+import System.Environment
+
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    ["ping", host, port, addr] -> do
+      Right transport <- createTransport host port defaultTCPParameters
+      localnode <- newLocalNode transport initRemoteTable
+      let nid = NodeId . EndPointAddress . pack $ addr
+      runProcess localnode (ping nid)
+    ["pong", host, port] -> do
+      Right transport <- createTransport host port defaultTCPParameters
+      localnode <- newLocalNode transport initRemoteTable
+      putStrLn $ "addr: " ++ show (nodeAddress $ localNodeId localnode)
+      runProcess localnode pong
+
+ping :: NodeId -> Process ()
+ping nid = do
+  self <- getSelfPid
+  registerRemoteAsync nid "ping" self
+  reply <- expect :: Process RegisterReply
+  say $ show reply
+  -- whereisRemoteAsync nid "pong"
+  -- reply' <- expect :: Process WhereIsReply
+  -- say $ show reply'
+  nsendRemote nid "pong" ("ping", self)
+  ("pong", pid) <- expect :: Process (String, ProcessId)
+  say $ show pid
+  liftIO $ threadDelay 10000
+
+pong :: Process ()
+pong = do
+  self <- getSelfPid
+  register "pong" self
+  ("ping", pid) <- expect :: Process (String, ProcessId)
+  -- send pid ("pong", self)
+  nsend "ping" ("pong", self)
