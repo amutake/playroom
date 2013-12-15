007b47a89952ce1ef08295d40ce862c519006d61
amutake <amutake.s@gmail.com>
2013-12-15 18:20:51 +0900

    Simplify

--- coq/bug.v
+++ coq/bug.v
@@ -2,12 +2,8 @@ Inductive A : Set := a : A.
 
 Definition fantom (n : nat) := A.
 
-Definition fantom_succ {n : nat} : fantom n -> fantom (S n) :=
-  fun _ => a.
-
-Inductive ty : forall n : nat, fantom n -> Prop :=
-| zero : ty O a
-| succ : forall (x : nat) (f : fantom x), ty x f -> ty (S x) (fantom_succ f).
+Inductive ty : forall n, fantom n -> Prop :=
+| zero : ty O a.
 
 Goal forall (n : nat) (f : fantom n), ty n f -> f = a.
 Proof.
