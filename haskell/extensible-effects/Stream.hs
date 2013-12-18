1fca63b7052a64e0fb6a702d6d4fc7bc4514075f
amutake <amutake.s@gmail.com>
2013-12-18 21:37:53 +0900

    Works, but broken

--- haskell/extensible-effects/Stream.hs
+++ haskell/extensible-effects/Stream.hs
@@ -9,6 +9,7 @@
   , PolymorphicComponents
   , LiberalTypeSynonyms
   , ScopedTypeVariables
+  , IncoherentInstances
   #-}
 
 module Stream where
@@ -17,24 +18,23 @@ import Control.Applicative
 import Data.Typeable
 
 import Control.Eff
-import Data.Void
 
 -- source :==: a :==: b :==: sink
 
 -- http://d.hatena.ne.jp/hiratara/20131029/1383053821
 
 data Stop = Stop
-data Await i v = Await (i -> Either Stop (Await i v)) (Stop -> v)
-data Yield o v = Yield o (Either Stop (Yield o v))
+data Await i v = Await (i -> v) (() -> v) deriving (Typeable, Functor)
+data Yield o v = Yield o v | Done v deriving (Typeable, Functor)
 
 -- send :: (forall w. (a -> VE w r) -> Union r (VE w r)) -> Eff r a
 -- inj  :: (Functor t, Typeable1 t, Member t r) => t v -> Union r v
 
 await :: (Typeable i, Member (Await i) r) => Eff r (Maybe i)
-await = send $ \f -> inj $ Await $ \i -> undefined
+await = send $ \f -> inj $ Await (f . Just) (\() -> f Nothing)
 
 yield :: (Typeable o, Member (Yield o) r) => o -> Eff r ()
-yield o = send $ \f -> inj $ Yield o $ undefined
+yield o = send $ \f -> inj $ Yield o (f ())
 
 produce :: (Typeable o, Member (Yield o) r) => [o] -> Eff r ()
 produce = mapM_ yield
@@ -52,36 +52,38 @@ combine :: (Typeable x, Member (Yield x) r1, Member (Await x) r2)
         -> Eff r v'
 combine = undefined
 
-runStream :: Eff (Yield x :> r) ()
+runStream :: (Typeable x)
+          => Eff (Yield x :> r) ()
           -> Eff (Await x :> r) v
           -> Eff r v
 runStream m1 m2 = loop (admin m1) (admin m2) -- admin m1 :: VE () (Yield x :> r), admin m2 :: VE v (Await x :> r)
   where
     -- loop :: VE () ((Yield x) :> r) -> VE v ((Await x) :> r) -> Eff r v
     loop _ (Val v) = return v -- v :: v
-    loop v@(Val _) (E u) = handleRelay u (loop v) $ \(Await _ stop) -> stop Stop -- u :: Union (Await x :> r) (VE v (Await x :> r))
+    loop v@(Val _) (E u) = handleRelay u (loop v) $ \(Await _ stop) -> loop v (stop ()) -- u :: Union (Await x :> r) (VE v (Await x :> r))
     -- u1 :: Union (Yield x :> r) (VE () (Yield x :> r)), u2 :: Union (Await x :> r) (VE v (Await x :> r))
-    loop (E u1) (E u2) = handleRelay u2 (loop (E u1)) $ \(Await next stop) -> do -- next :: x -> Either Stop (Await i (VE v (Await x :> r))), stop :: Stop -> VE v (Await x :> r)
+    loop (E u1) (E u2) = handleRelay u2 (loop (E u1)) $ \(Await next stop) -> do -- next :: x -> VE v (Await x :> r), stop :: () -> VE v (Await x :> r)
                       -- handleRelay :: Union (Await x :> r) (VE v (Await x :> r))
                       --             -> (VE v (Await x :> r) -> Eff r v)
                       --             -> (Await x (VE v (Await x :> r)) -> Eff r v)
                       --             -> Eff r v
-      handleRelay u1 (\ve -> loop ve (stop Stop)) $ \(Yield x next') -> -- x :: x, next :: Either Stop (Yield x (VE () (Yield x :> r)))
+      handleRelay u1 (\ve -> loop ve (stop ())) $ \y -> case y of -- x :: x, next :: Either Stop (Yield x (VE () (Yield x :> r)))
         -- handleRelay :: Union (Yield x :> r) (VE () (Yield x :> r))
         --             -> (VE () (Yield x :> r) -> Eff r v)
         --             -> (Yield x (VE () (Yield x :> r)) -> Eff r v)
         --             -> Eff r v
-        case next' of
-          Left Stop -> next x
-        loop $ extract $ f x -- :: Eff r (VE r (Yield x :> r))
+        Yield x v -> loop v (next x)
+        Done v -> loop v (stop ())
+    -- go (Done ve) = loop ve
+    -- go (Await cont) = loop $ extract $ cont ()
+    -- go (Yield _ stream) = loop $ extract stream
+    -- -- extract :: (:==:) () o v -> v
+    -- extract (Done v) = v
+    -- extract (Await cont) = extract $ cont ()
+    -- extract (Yield _ stream) = extract stream
 
-    go (Done ve) = loop ve
-    go (Await cont) = loop $ extract $ cont ()
-    go (Yield _ stream) = loop $ extract stream
-    -- extract :: (:==:) () o v -> v
-    extract (Done v) = v
-    extract (Await cont) = extract $ cont ()
-    extract (Yield _ stream) = extract stream
+(|==|) :: (Typeable x) => Eff (Yield x :> r) () -> Eff (Await x :> r) v -> Eff r v
+(|==|) = runStream
 
 {-
 admin :: Eff r w -> VE w r
@@ -100,5 +102,8 @@ handleRelay :: Typeable1 t
 
 main :: IO ()
 main = do
-  let eff = runStream (produce [1..10]) consume
-  print $ run eff
+  let src = produce ([1..10] :: [Int]) :: Eff (Yield Int :> Yield Int :> ()) ()
+      con = mapS ((* 2) :: Int -> Int) :: Eff (Await Int :> Yield Int :> ()) ()
+      sin = consume :: Eff (Await Int :> ()) [Int]
+      eff = run $ (src |==| con) |==| sin
+  print eff
