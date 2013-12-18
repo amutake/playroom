7a61be82d5c4b8135110e37c3acdceadc8ff7289
amutake <amutake.s@gmail.com>
2013-12-19 00:13:38 +0900

    unsafeReUnion

--- haskell/extensible-effects/Stream.hs
+++ haskell/extensible-effects/Stream.hs
@@ -24,14 +24,14 @@ import Control.Eff
 -- http://d.hatena.ne.jp/hiratara/20131029/1383053821
 
 data Stop = Stop
-data Await i v = Await (i -> v) (() -> v) deriving (Typeable, Functor)
+data Await i v = Await (i -> v) v deriving (Typeable, Functor)
 data Yield o v = Yield o v | Done v deriving (Typeable, Functor)
 
 -- send :: (forall w. (a -> VE w r) -> Union r (VE w r)) -> Eff r a
 -- inj  :: (Functor t, Typeable1 t, Member t r) => t v -> Union r v
 
 await :: (Typeable i, Member (Await i) r) => Eff r (Maybe i)
-await = send $ \f -> inj $ Await (f . Just) (\() -> f Nothing)
+await = send $ \f -> inj $ Await (f . Just) $ f Nothing
 
 yield :: (Typeable o, Member (Yield o) r) => o -> Eff r ()
 yield o = send $ \f -> inj $ Yield o (f ())
@@ -54,41 +54,55 @@ combine = undefined
 
 infixl 8 |==|
 (|==|) :: (Typeable x)
-       => Eff (Yield x :> r) ()
+       => Eff (Yield x :> r') ()
        -> Eff (Await x :> r) v
        -> Eff r v
-(|==|) m1 m2 = loop (admin m1) (admin m2) -- admin m1 :: VE () (Yield x :> r), admin m2 :: VE v (Await x :> r)
+m1 |==| m2 = loop (admin m1) (admin m2) -- admin m1 :: VE () (Yield x :> r), admin m2 :: VE v (Await x :> r)
   where
-    -- loop :: VE () ((Yield x) :> r) -> VE v ((Await x) :> r) -> Eff r v
+    -- loop :: VE () ((Yield x) :> r') -> VE v ((Await x) :> r) -> Eff r v
     loop _ (Val v) = return v -- v :: v
-    loop v@(Val _) (E u) = handleRelay u (loop v) $ \(Await _ stop) -> loop v (stop ()) -- u :: Union (Await x :> r) (VE v (Await x :> r))
-    -- u1 :: Union (Yield x :> r) (VE () (Yield x :> r)), u2 :: Union (Await x :> r) (VE v (Await x :> r))
-    loop (E u1) (E u2) = handleRelay u2 (loop (E u1)) $ \(Await next stop) -> do -- next :: x -> VE v (Await x :> r), stop :: () -> VE v (Await x :> r)
-                      -- handleRelay :: Union (Await x :> r) (VE v (Await x :> r))
-                      --             -> (VE v (Await x :> r) -> Eff r v)
-                      --             -> (Await x (VE v (Await x :> r)) -> Eff r v)
-                      --             -> Eff r v
-      handleRelay u1 (\ve -> loop ve (stop ())) $ \y -> case y of -- x :: x, next :: Either Stop (Yield x (VE () (Yield x :> r)))
-        -- handleRelay :: Union (Yield x :> r) (VE () (Yield x :> r))
-        --             -> (VE () (Yield x :> r) -> Eff r v)
-        --             -> (Yield x (VE () (Yield x :> r)) -> Eff r v)
-        --             -> Eff r v
-        Yield x v -> loop v (next x)
-        Done v -> loop v (stop ())
-    -- go (Done ve) = loop ve
-    -- go (Await cont) = loop $ extract $ cont ()
-    -- go (Yield _ stream) = loop $ extract stream
-    -- -- extract :: (:==:) () o v -> v
-    -- extract (Done v) = v
-    -- extract (Await cont) = extract $ cont ()
-    -- extract (Yield _ stream) = extract stream
+    loop v@(Val _) (E u) = handleRelay u (loop v) $ \(Await _ stop) -> loop v stop
+    -- u1 :: Union (Yield x :> r') (VE () (Yield x :> r'))
+    -- u2 :: Union (Await x :> r) (VE v (Await x :> r))
+    loop (E u1) (E u2) = case decomp u2 of
+      Left u2' -> send (\f -> f <$> u2') >>= loop (E u1)
+                -- u :: Union r (VE v (Await x :> r))
+                -- send :: (forall w. (v -> VE w r) -> Union r (VE w r)) -> Eff r v
+      Right (Await next stop) -> case decomp u1 of
+                 -- :: Await x (VE v (Await x :> r))
+                 -- next :: x -> VE v (Await x :> r)
+                 -- stop :: VE v (Await x :> r)
+        Left u1' -> send (\f -> f <$> unsafeReUnion u1') >>= flip loop stop
+          -- u1' :: Union r' (VE () (Yield x :> r'))
+          -- send :: (forall w. (v -> VE w r) -> Union r (VE w r)) -> Eff r v
+        Right (Yield x v) -> loop v (next x)
+        Right (Done v) -> loop v stop
 
+      -- handleRelay u2 (loop (E u1)) $ \(Await next stop) -> do
+      -- handleRelay :: Union (Await x :> r) (VE v (Await x :> r))
+      --             -> (VE v (Await x :> r) -> Eff r v)
+      --             -> (Await x (VE v (Await x :> r)) -> Eff r v)
+      --             -> Eff r v
+      -- v <- handleRelay u1 (\ve -> loop ve stop) $ \y -> case y of
+      --   -- y :: Yield x (VE () (Yield x :> r'))
+      --   -- handleRelay :: Union (Yield x :> r) (VE () (Yield x :> r))
+      --   --             -> (VE () (Yield x :> r) -> Eff r v)
+      --   --             -> (Yield x (VE () (Yield x :> r)) -> Eff r v)
+      --   --             -> Eff r v
+      --   Yield x v -> loop v (next x)
+      --   -- x :: x, v :: (VE () (Yield x :> r'))
+      --   Done v -> loop v stop
+      -- loop v stop
 {-
 admin :: Eff r w -> VE w r
 
 data VE w r
   = Val w
   | E !(Union r (VE w r))
+
+decomp :: Typeable1 t => Union (t :> r) v -> Either (Union r v) (t v)
+
+send :: (forall w. (a -> VE w r) -> Union r (VE w r)) -> Eff r a
 
 handleRelay :: Typeable1 t
             => Union (t :> r) v
@@ -100,8 +114,8 @@ handleRelay :: Typeable1 t
 
 main :: IO ()
 main = do
-  let src = produce ([1..10] :: [Int]) :: Eff (Yield Int :> Yield Int :> ()) ()
+  let src = produce ([1..10] :: [Int]) :: Eff (Yield Int :> ()) ()
       con = mapS ((* 2) :: Int -> Int) :: Eff (Await Int :> Yield Int :> ()) ()
-      sin = consume :: Eff (Await Int :> ()) [Int]
-      eff = run $ src |==| con |==| sin
+      snk = consume :: Eff (Await Int :> ()) [Int]
+      eff = run $ src |==| con |==| snk
   print eff
