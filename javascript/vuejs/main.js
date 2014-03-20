b148c1b8c31cb4850f0acc2f0eb8357603981c31
amutake <amutake.s@gmail.com>
2014-03-20 19:05:32 +0900

    Modify main.js

--- javascript/vuejs/main.js
+++ javascript/vuejs/main.js
@@ -1,3 +1,40 @@
+var api = {
+    fetch: function() {
+        var self = this;
+        $.ajax({
+            url: "http://localhost:3000/todos",
+            type: "GET"
+        }).done(function(todos) {
+            self.todos = todos;
+        });
+    },
+    add: function() {
+        var self = this;
+        $.ajax({
+            url: "http://localhost:3000/todos",
+            type: "POST",
+            data: JSON.stringify(self.form)
+        }).done(function(todo) {
+            self.todos.push(todo);
+            self.form.content = "";
+        });
+    },
+    toggle: function(id) {
+        var self = this;
+        $.ajax({
+            url: "http://localhost:3000/todos/toggle",
+            type: "POST",
+            data: {
+                id: id
+            }
+        }).done(function(ok) {
+            if (ok) {
+                self.todos[id].done = !self.todos[id].done
+            }
+        });
+    }
+};
+
 var vm = new Vue({
     el: '#demo',
     data: {
@@ -9,44 +46,9 @@ var vm = new Vue({
         }
     },
     methods: {
-        fetch: function() {
-            var self = this;
-            var endpoint = "http://localhost:3000/todos";
-            $.ajax({
-                url: endpoint,
-                type: "GET"
-            }).done(function(todos) {
-                self.todos = todos;
-            });
-        },
-        add: function() {
-            var self = this;
-            var endpoint = "http://localhost:3000/todos";
-            var todo = self.form;
-            $.ajax({
-                url: endpoint,
-                type: "POST",
-                data: JSON.stringify(todo)
-            }).done(function(todo) {
-                self.fetch();
-                self.form.content = "";
-            });
-        },
-        toggle: function(id) {
-            var self = this;
-            var endpoint = "http://localhost:3000/todos/toggle";
-            $.ajax({
-                url: endpoint,
-                type: "POST",
-                data: {
-                    id: id
-                }
-            }).done(function(ok) {
-                if (ok) {
-                    self.todos[id].done = !self.todos[id].done
-                }
-            });
-        }
+        fetch: api.fetch,
+        add: api.add,
+        toggle: api.toggle
     },
     created: function() {
         this.fetch();
