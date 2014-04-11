2b3e6b04b27b069641ff5cc7133cccf164b3bbac
amutake <amutake.s@gmail.com>
2014-04-11 21:35:27 +0900

    More ex2.v

--- coq/coqex2014/ex2.v
+++ coq/coqex2014/ex2.v
@@ -28,3 +28,115 @@ Proof.
   apply H with x.
   assumption.
 Qed.
+
+Goal forall m n : nat, (n * 10) + m = (10 * n) + m.
+Proof.
+  intros.
+  rewrite mult_comm.
+  reflexivity.
+Qed.
+
+Goal forall n m p q : nat, (n + m) + (p + q) = (n + p) + (m + q).
+Proof.
+  intros.
+  rewrite plus_assoc.
+  replace (n + m + p) with (n + p + m). (* いい感じに rewrite する方法がわかりません *)
+    rewrite <- plus_assoc.
+    reflexivity.
+
+    rewrite <- plus_assoc.
+    replace (p + m) with (m + p).
+      rewrite plus_assoc.
+      reflexivity.
+
+      apply plus_comm.
+Qed.
+
+Lemma plus_l : forall n m p, n = m -> p + n = p + m.
+Proof.
+  intros; rewrite H; reflexivity.
+Qed.
+
+Goal forall n m : nat, (n + m) * (n + m) = n * n + m * m + 2 * n * m.
+Proof.
+  intros.
+  rewrite mult_plus_distr_l.
+  rewrite mult_plus_distr_r.
+  rewrite <- plus_assoc.
+  rewrite <- plus_assoc.
+  apply plus_l.
+  rewrite mult_plus_distr_r.
+
+  rewrite plus_comm.
+  replace (n * m + m * m) with (m * m + n * m).
+    rewrite <- plus_assoc.
+    apply plus_l.
+    replace (m * n) with (n * m).
+      simpl.
+      rewrite mult_plus_distr_r.
+      rewrite mult_plus_distr_r.
+      rewrite mult_0_l.
+      rewrite plus_0_r.
+      reflexivity.
+
+      apply mult_comm.
+    apply plus_comm.
+Qed.
+
+Parameter G : Set.
+Parameter mult : G -> G -> G.
+Notation "x * y" := (mult x y).
+Parameter one : G.
+Notation "1" := one.
+Parameter inv : G -> G.
+Notation "/ x" := (inv x).
+
+Axiom mult_assoc : forall x y z, x * (y * z) = (x * y) * z.
+Axiom one_unit_l : forall x, 1 * x = x.
+Axiom inv_l : forall x, /x * x = 1.
+
+Lemma mult_eq_l_1 : forall x y z, x = y -> z * x = z * y.
+Proof.
+  intros.
+  rewrite H.
+  reflexivity.
+Qed.
+
+Lemma mult_eq_l_2 : forall x y z, z * x = z * y -> x = y.
+Proof.
+  intros.
+  apply mult_eq_l_1 with (z := /z) in H.
+  rewrite mult_assoc in H.
+  rewrite mult_assoc in H.
+  rewrite inv_l in H.
+  rewrite one_unit_l in H.
+  rewrite one_unit_l in H.
+  assumption.
+Qed.
+
+Lemma mult_one_comm : forall x, 1 * x = x * 1.
+Proof.
+  intros.
+  rewrite one_unit_l.
+  apply mult_eq_l_2 with (/x).
+  rewrite mult_assoc.
+  rewrite inv_l.
+  rewrite one_unit_l.
+  reflexivity.
+Qed.
+
+Lemma inv_r : forall x, x * / x = 1.
+Proof.
+  intros.
+  apply mult_eq_l_2 with (z := /x).
+  rewrite mult_assoc.
+  rewrite inv_l.
+  apply mult_one_comm.
+Qed.
+
+Lemma one_unit_r : forall x, x * 1 = x.
+Proof.
+  intros.
+  rewrite <- mult_one_comm.
+  apply one_unit_l.
+Qed.
