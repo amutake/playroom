070a42049362c47c39dbd8512bf7bc3b99eda591
amutake <amutake.s@gmail.com>
2014-05-14 12:20:40 +0900

    modify stream

--- haskell/effin/Stream.hs
+++ haskell/effin/Stream.hs
@@ -27,11 +27,9 @@ type family YieldType es where
 
 await :: (EffectAwait i es) => Effect es (Maybe i)
 await = send $ Await Just Nothing
--- Member (Await i) es => Await i (Maybe i) -> Effect es (Maybe i)
 
 yield :: (EffectYield o es) => o -> Effect es ()
-yield o = send $ Yield o ()
--- Member (Yield o) es => Yield o () -> Effect es ()
+yield = send . flip Yield ()
 
 produce :: (EffectYield o es) => [o] -> Effect es ()
 produce = mapM_ yield
@@ -42,52 +40,32 @@ consume = await >>= maybe (return []) (\i -> (i :) <$> consume)
 awaitForever :: (EffectAwait i es) => (i -> Effect es v) -> Effect es ()
 awaitForever f = await >>= maybe (return ()) (\i -> f i >> awaitForever f)
 
-runStream :: (Show x, Show a, Show b) => Effect (Yield x ': es) a
+runStream :: Effect (Yield x ': es) a
           -> Effect (Await x ': es) b
           -> Effect es b
 runStream y a = handle return awaitHandler a
   where
---    awaitHandler :: Handler (Await x ': es) (Effect es b)
-    awaitHandler = eliminate extractAwait defaultRelay
---    eliminate :: (Await x (Effect es v) -> Effect es v)
---              -> Handler es (Effect es v)
---              -> Handler (Await x : es) (Effect es v)
---    defaultRelay :: Handler es (Effect es v)
-    extractAwait :: Await x (Effect es b) -> Effect es b
-    extractAwait (Await f v) = handle (const v) (yieldHandler f) y
-    yieldHandler :: (x -> Effect es b) -> Handler (Yield x ': es) (Effect es b)
-    yieldHandler f = eliminate (extractYield f) $ relay yieldRelay
-    extractYield :: (x -> Effect es b) -> Yield x (Effect es (x -> b)) -> Effect es (x -> b)
-    extractYield f (Yield o v) = v $ f o
---    yieldRelay :: Member e es => e ((x -> Effect es b) -> Effect es b) -> (x -> Effect es b) -> Effect es b
-    yieldRelay e f = _ -- sendEffect $ fmap ($ f) e
+    -- awaitHandler :: Handler (Await x ': es) (Effect es b)
+    awaitHandler = eliminate bindAwait defaultRelay
 
--- runS :: Effect (Yield x ': es) a
---      -> Effect (Await x ': es) b
---      -> Effect es b
--- runS y a = handle return awaitHandler a
---   where
---     awaitHandler = eliminate extractAwait defaultRelay
---     extractAwait (Await f v) = handle (const v) (yieldHandler f) y
---     yieldHandler f = eliminate (extractYield f) defaultRelay
---     extractYield f (Yield o v) =
+    -- bindAwait :: Await x (Effect es b) -> Effect es b
+    bindAwait (Await f v) = handle (const v) (yieldHandler f) y
+
+    yieldHandler :: (x -> Effect es b) -> Handler (Yield x ': es) (Effect es b)
+    yieldHandler f = eliminate (bindYield f) defaultRelay
+
+    bindYield :: (x -> Effect es b) -> Yield x (Effect es b) -> Effect es b
+    bindYield f (Yield o v) = do
+        something <- f o
+        something v
+    -- f o が次の await
+    -- v が次の yield
+    -- await してから yield された値を持ってくる
 
 example :: IO ()
-example = do
-    b <- runLift $ runStream producer consumer
-    print b
+example = runLift $ runStream producer consumer
   where
     producer = do
         yield "hoge"
-        return False
-        -- yield "hoge"
-        -- yield "fuga"
-        -- return True
-    consumer = do
-        a <- await
-        lift $ maybe (putStrLn "nothing") putStrLn (a :: Maybe String)
-        b <- await
-        lift $ print b
-        return True
-        -- awaitForever $ \str -> do
-        --     lift $ putStrLn str
+        yield "fuga"
+    consumer = awaitForever $ lift . putStrLn
