3f892e5b2150cc04faa9266a4a3cfa894d774bb1
amutake <amutake.s@gmail.com>
2014-05-20 10:43:42 +0900

    church list

--- haskell/church/Types.hs
+++ haskell/church/Types.hs
@@ -1,6 +1,10 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RankNTypes, MultiParamTypeClasses, FlexibleInstances #-}
 
 module Types where
+
+class ChurchEncoding a b where -- a is church encoding of b
+    fromChurch :: a -> b
+    toChurch :: b -> a
 
 data ChurchNat = ChurchNat { unChurchNat :: forall e. e -> (e -> e) -> e }
 -- type Nat = forall e. e -> (e -> e) -> e
@@ -11,11 +15,22 @@ zero = ChurchNat const
 succ' :: ChurchNat -> ChurchNat
 succ' n = ChurchNat $ \z s -> s (unChurchNat n z s)
 
-int2nat :: Int -> ChurchNat
-int2nat n
-    | n < 0 = error "must be a positive number"
-    | n == 0 = zero
-    | otherwise = succ' $ int2nat $ pred n
+instance Integral n => ChurchEncoding ChurchNat n where
+    fromChurch n = unChurchNat n 0 succ
+    toChurch n
+        | n < 0 = error "must be a positive number"
+        | n == 0 = zero
+        | otherwise = succ' $ toChurch $ pred n
 
-nat2int :: ChurchNat -> Int
-nat2int n = unChurchNat n 0 succ
+data ChurchList a = ChurchList { unChurchList :: forall e. e -> (a -> e -> e) -> e }
+
+nil :: ChurchList a
+nil = ChurchList const
+
+cons :: a -> ChurchList a -> ChurchList a
+cons x xs = ChurchList $ \n c -> c x (unChurchList xs n c)
+
+instance ChurchEncoding (ChurchList a) [a] where
+    fromChurch xs = unChurchList xs [] (:)
+    toChurch [] = nil
+    toChurch (x:xs) = cons x (toChurch xs)
--- haskell/church/test.hs
+++ haskell/church/test.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Main where
 
 import Test.SmallCheck
@@ -6,4 +8,17 @@ import Test.SmallCheck.Series
 import Types
 
 testNat :: IO ()
-testNat = smallCheck 100 $ \(Positive n) -> n == (nat2int . int2nat) n
+testNat = smallCheck 100 $ \(NonNegative n) -> n == (from . to) n
+  where
+    to :: Int -> ChurchNat
+    to = toChurch
+    from :: ChurchNat -> Int
+    from = fromChurch
+
+testList :: IO ()
+testList = smallCheck 5 $ \xs -> xs == (from . to) xs
+  where
+    to :: [Int] -> ChurchList Int
+    to = toChurch
+    from :: ChurchList Int -> [Int]
+    from = fromChurch
