0ab20fa79e39035a815f0be5e1ba3307edc2b2ed
amutake <amutake.s@gmail.com>
2014-11-08 10:13:35 +0900

    Add javascript files

---
+++ javascript/socket.io/app.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+
+var handler = function(req, res) {
+    fs.readFile(__dirname + '/index.html', function(err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+};
+
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+
+io.on('connection', function(socket) {
+    socket.emit('news', {
+        hello: 'world'
+    });
+    socket.on('my other event', function(data) {
+        console.log(data);
+    });
+});
