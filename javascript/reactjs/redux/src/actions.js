80b51dd0bdcafba368929ad6eb07868461dfde49
amutake <amutake.s@gmail.com>
2015-08-17 11:25:34 +0900

    truly async

--- javascript/reactjs/redux/src/actions.js
+++ javascript/reactjs/redux/src/actions.js
@@ -1,8 +1,12 @@
 export function loadItems(dispatch) {
   new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(['hoge', 'fuga', 'piyo']);
+    }, 500);
+  }).then(items => {
     dispatch({
       type: 'SET_ITEMS',
-      items: ['hoge', 'fuga', 'piyo']
+      items
     });
   });
 }
